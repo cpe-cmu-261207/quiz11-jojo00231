a3e5ae6719c5b6c23805ad2a2145e1755104dabd
jojo69 <jojo00231.jj@gmail.com>
2021-01-12 14:00:39 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,8 @@
-//your code here!
+
+
+const body = document.body
+const div = document.querySelector("div")
+const calculator = document.querySelector("#cal")
+const number = document.querySelector("#input-number")
+
+calculator.title = 620610813-number.placeholder
